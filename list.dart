bd567328e59be021caf73d7f6c91f2be14a0b6cc
OnelKavindhu <106512102+OnelKavindhu@users.noreply.github.com>
2025-01-19 20:49:44 +0530

    second  commit

--- list.dart
+++ list.dart
@@ -1,14 +1,6 @@
-import 'dart:io';
-
 void main() {
-  print("Enter the number of subjects : ");
-  int? j = int.parse(stdin.readLineSync()!);
-
-  for (i = 0; i <= j; i++);
-
-  List marks = [];
-  print("Enter you maths marks: ");
-  int? maths = int.parse(stdin.readLineSync()!);
-  marks.add(maths);
-  print(marks);
+  List subjects = ["English", "Maths", "IT", "Sinhala", "History"];
+  for (int i = 0; i < 5; i++) {
+    print("Enter ${subjects[i]} marks: ");
+  }
 }
